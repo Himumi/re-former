a9b54379abf854997605cc69738167d1b79c1d5f
himumi <erliansyaha@gmail.com>
2024-06-26 20:36:32 +0900

    refactor: add some parts for write form to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(allowed_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+    def allowed_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
